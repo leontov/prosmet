57ecbc18f1076be6357249cef35a83ddfedc3113
Vladislav <cochurov.vlad@gmail.com>
2026-07-31 21:56:48 +0300

    fix(mobile): use portable absolute drawer geometry

--- apps/mobile/src/MobileNavigation.tsx
+++ apps/mobile/src/MobileNavigation.tsx
@@ -58,7 +58,11 @@ export function MobileNavigation({ screen, onNavigate, onClose }: Props) {
 
 const styles = StyleSheet.create({
   layer: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
     zIndex: 100,
     flexDirection: "row",
     justifyContent: "flex-end",
